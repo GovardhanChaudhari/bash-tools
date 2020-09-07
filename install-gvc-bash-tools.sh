5957bfce3b9f42edeb173138109dbdd64edb1891
govardhanchaudhari@gmail.com <govardhanchaudhari.gmail.com>
2020-09-07 19:27:45 +0530

    Fix: path bug

--- install-gvc-bash-tools.sh
+++ install-gvc-bash-tools.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-dest_dir="~/bash-tools"
+dest_dir="$HOME/bash-tools"
 alia_file="bash_aliases.txt"
 if [[ -d $dest_dir ]] ; then
   echo "Directory '$dest_dir' exists, skiping git clonning"
 else
-  git clone https://github.com/GovardhanChaudhari/bash-tools.git  $dest_dir
+  git clone https://github.com/GovardhanChaudhari/bash-tools.git  "$dest_dir"
 fi
-final_path=$dest_dir/$alia_file
+final_path="$dest_dir/$alia_file"
 echo $final_path
 source $final_path
