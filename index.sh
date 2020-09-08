d76d4f25a020cccef72b3cb1fffc2011f1bedf25
govardhanchaudhari@gmail.com <govardhanchaudhari.gmail.com>
2020-09-08 16:17:52 +0530

    Add:

---
+++ index.sh
@@ -0,0 +1,5 @@
+parent_dir="$HOME/bash-tools"
+source $parent_dir/bash_aliases.txt
+source $parent_dir/gvc-file-utils.txt
+source $parent_dir/gvc-command-utils.txt
+source $parent_dir/gvc-git-functions.sh
