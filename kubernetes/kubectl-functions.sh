4dca0a0c7b03d8bc6cfee090f38ae8952dd2fdca
govardhanchaudhari@gmail.com <govardhanchaudhari.gmail.com>
2020-09-11 21:01:09 +0530

    Update:

--- kubernetes/kubectl-functions.sh
+++ kubernetes/kubectl-functions.sh
@@ -2,6 +2,7 @@
 kubectl_command="kubectl"
 kubectl_get_command="$kubectl_command get"
 kubectl_config_command="$kubectl_command config"
+kubectl_port_forward_command="$kubectl_command port-forward"
 
 #List namespaces
 gkln(){
@@ -17,4 +18,14 @@ gklp(){
 gkscn(){
     export CONTEXT=$($kubectl_config_command view | awk '/current-context/ {print $2}')
     $kubectl_config_command set-context $CONTEXT --namespace="$1"
+}
+
+# Launch dashboard
+gkdb(){
+    $kubectl_command proxy
+}
+
+#Launch weav dashboard
+gkwd(){
+    $kubectl_port_forward_command -n weave "$($kubectl_get_command -n weave pod --selector=weave-scope-component=app -o jsonpath='{.items..metadata.name}')" 4040
 }
