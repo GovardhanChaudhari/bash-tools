ca5d1fdc71910d98ea11e4903018d6d6e1f222ab
govardhanchaudhari@gmail.com <govardhanchaudhari.gmail.com>
2020-09-11 20:00:59 +0530

    Add: kubertnetes functions: gkln, gkscn

--- index.sh
+++ index.sh
@@ -7,3 +7,6 @@ source $parent_dir/bash_aliases.sh
 source $parent_dir/gvc-file-utils.sh
 source $parent_dir/gvc-command-utils.sh
 source $parent_dir/gvc-git-functions.sh
+
+#Kubernetes Functions
+source $parent_dir/kubernetes/kubectl-functions.sh
---
+++ kubernetes/kubectl-functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+kubectl_command="kubectl"
+kubectl_get_command="$kubectl_command get"
+kubectl_config_command="$kubectl_command config"
+
+#List namespaces
+gkln(){
+    $kubectl_get_command namespaces
+}
+
+# List pods
+gkgp(){
+    $kubectl_get_command pods
+}
+
+#Set current namespace
+gkscn(){
+    export CONTEXT=$($kubectl_config_command view | awk '/current-context/ {print $2}')
+    $kubectl_config_command set-context $CONTEXT --namespace="$1"
+}
