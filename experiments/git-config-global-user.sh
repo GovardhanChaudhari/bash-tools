012393eaeaada90e01936fdd851d40a4941948d8
govardhanchaudhari <govardhanchaudhari@gmail.com>
2020-09-18 23:48:38 +0530

    Add:

---
+++ experiments/git-config-global-user.sh
@@ -0,0 +1,4 @@
+git config --global credential.helper store
+git config --global user.email govardhanchaudhari@gmail.com
+git config --global  user.name GovardhanChaudhari
+echo "Note: you need to add user name and password for first time when prompted"
