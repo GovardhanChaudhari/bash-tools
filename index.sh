d9bce348bbf2312ecc3fbbb7c2d5ff985bf66619
govardhanchaudhari <govardhanchaudhari@gmail.com>
2020-09-20 15:14:27 +0530

    Moved

--- index.sh
+++ index.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-source $HOME/bash-tools/env_vars.sh
+source $HOME/bash-tools/env/env_vars.sh
 
 parent_dir="$GVC_BASH_TOOLS_HOME"
 
-source $parent_dir/utils.sh
-source $parent_dir/bash_aliases.sh
-source $parent_dir/gvc-file-utils.sh
-source $parent_dir/gvc-command-utils.sh
-source $parent_dir/gvc-git-functions.sh
+source $parent_dir/utils/utils.sh
+source $parent_dir/bash/bash_aliases.sh
+source $parent_dir/file/gvc-file-utils.sh
+source $parent_dir/command/gvc-command-utils.sh
+source $parent_dir/git/gvc-git-functions.sh
 
 #Kubernetes Functions
 source $parent_dir/kubernetes/kubectl-functions.sh
