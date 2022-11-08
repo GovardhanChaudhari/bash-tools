8957b6b386fd8d0f4bd6092a5a249c8adb73b94b
Govardhan Chaudhari <govardhanchaudhari@gmail.com>
2022-11-08 22:05:56 +0530

    added apt shortcuts

---
+++ apt/apt.sh
@@ -0,0 +1,21 @@
+# base command
+aptcmd(){ sudo apt $@ }
+
+# install
+gi(){ aptcmd install $@ }
+
+# update
+gu(){ aptcmd update $@ }
+
+# upgrade 
+gup(){ aptcmd upgrade $@ }
+
+# full upgrade
+gfup(){ aptcmd fullupgrade $@}
+
+# list upgradable
+glu(){ aptcmd list --upgradable }
+
+# autoremove
+gar(){ aptcmd autoremove $@ }
+
